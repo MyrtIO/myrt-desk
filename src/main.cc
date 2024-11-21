45edf450c86a6fd85cbc444180bc2e0533553b4f
mishamyrt <misha@myrt.co>
2024-11-21 20:54:08 +0300

    feat: rework initialization

--- src/main.cc
+++ src/main.cc
@@ -2,31 +2,24 @@
 #include "controllers/controllers.h"
 #include "platforms/platforms.h"
 
-#ifdef IO_BENCHMARK
-#include <MyrtIO/benchmarking/lps.h>
-#endif
-
 io::Device desk("Desk");
 
 void setup() {
+	// Enable MQTT logging
 	auto mqttController = IO_INJECT_INSTANCE(MQTTController);
 	io::Logger::setOutput(mqttController->logStream());
+
 	desk.setup()
-	    ->platforms(
-			IO_INJECT_UNIT(IWiFiPlatform),
-			IO_INJECT_UNIT(IHeightPlatform),
-			IO_INJECT_UNIT(ILightPlatform)
-		)
-	    ->controllers(
-			IO_INJECT_UNIT(BootController),
-			IO_INJECT_UNIT(MQTTController),
-			IO_INJECT_UNIT(OTAController)
-		);
-#ifdef IO_BENCHMARK
-	io::LPSBenchmark lpsBenchmark;
-	desk.controllers(&lpsBenchmark);
-#endif
-	desk.log()->print("initialized");
+		// Add platforms
+		.registerPlatform(IO_INJECT_UNIT(IWiFiPlatform))
+		.registerPlatform(IO_INJECT_UNIT(ILightPlatform))
+		.registerPlatform(IO_INJECT_UNIT(IHeightPlatform))
+		// Add controllers
+		.registerController(IO_INJECT_UNIT(BootController))
+		.registerController(IO_INJECT_UNIT(MQTTController))
+		.registerController(IO_INJECT_UNIT(OTAController));
+
+	desk.log().print("initialized");
 }
 
 void loop() {
